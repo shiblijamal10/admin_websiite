ef3ef783601288e73fefedebe69369717ca79883
shiblijamal <shiblijamal10@gmail.com>
2023-12-03 21:48:02 +0530

    p1

---
+++ app/lib/data.js
@@ -0,0 +1,69 @@
+import {User,Product} from "./models"
+import { connectToDB } from "./utils";
+
+export const fetchUsers = async (q,page) => {
+//   console.log(id);
+const regex= new RegExp(q,"i");
+
+const ITEM_PER_PAGE = 2;
+
+  try {
+    connectToDB();
+    const count = await User.find({ username: { $regex: regex } }).count();
+    const users = await User.find({ username: { $regex: regex } })
+      .limit(ITEM_PER_PAGE)
+      .skip(ITEM_PER_PAGE * (page - 1));
+    return { count, users };
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to fetch users!");
+  }
+};
+
+
+export const fetchProducts = async (q, page) => {
+  console.log(q);
+  const regex = new RegExp(q, "i");
+
+  const ITEM_PER_PAGE = 2;
+
+  try {
+    connectToDB();
+    const count = await Product.find({ title: { $regex: regex } }).count();
+    const products = await Product.find({ title: { $regex: regex } })
+      .limit(ITEM_PER_PAGE)
+      .skip(ITEM_PER_PAGE * (page - 1));
+    return { count, products };
+  } catch (err) {
+    console.log(err);
+    // throw new Error("Failed to fetch products!");
+  }
+};
+
+
+
+export const fetchUser = async (id) => {
+  console.log(id);
+  try {
+    connectToDB();
+    const user = await User.findById(id);
+    return user;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to fetch user!");
+  }
+};
+
+
+
+
+export const fetchProduct = async (id) => {
+  try {
+    connectToDB();
+    const product = await Product.findById(id);
+    return product;
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to fetch product!");
+  }
+};
